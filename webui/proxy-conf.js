995806e84eec299a274e12d671224b7d199212ca
Fabien Meurillon <fabien.meurillon@corp.ovh.com>
2018-06-04 16:43:38 +0200

    Minor UI fixes
    Favicon was not showing on preview (401)
    make live was not handling assets properly
    Minor CDS workflow change to mitigate CDS bug
    Bump dependencies

--- webui/proxy-conf.js
+++ webui/proxy-conf.js
@@ -5,33 +5,38 @@
  * @param {*} proxyOptions
  */
 const redirect = function (req, res, proxyOptions) {
-    console.log("Catch all.", req.originalUrl);
-    res.set('location', 'http://localhost:4200/?redirect='+req.originalUrl.replace('?','&'));
-    res.status(301)
+  console.log("Catch all.", req.originalUrl);
+  res.set('location', 'http://localhost:4200/?redirect=' + req.originalUrl.replace('?', '&'));
+  res.status(301)
 };
 
 const injectCreds = function (req, res, proxyOptions) {
-    console.log("Inject.");
-    req.headers["X-Remote-User"] = "fabien.meurillon";
+  console.log("Inject.");
+  req.headers["X-Remote-User"] = "fabien.meurillon";
 };
 
 const PROXY_CONFIG = {
-    "/api": {
-        "target": "http://localhost:8081",
-        "secure": false
-     },
-     "/uservice/gateway/appcatalog/api": {
-        "target": "http://localhost:8081",
-        "secure": false,
-        "changeOrigin": true,
-        "logLevel": "info",
-        "pathRewrite": {"^/uservice/gateway/appcatalog/api" : "/api"},
-        "bypass": injectCreds
-     },
-     "/all": {
-        "secure": false,
-        "bypass": redirect
-    },
+  "/api": {
+    "target": "http://localhost:8081",
+    "secure": false
+  },
+  "/ui//assets": {
+    "target": "http://localhost:4200",
+    "pathRewrite": {"^/ui//assets": "/assets"},
+    "secure": false
+  },
+  "/uservice/gateway/appcatalog/api": {
+    "target": "http://localhost:8081",
+    "secure": false,
+    "changeOrigin": true,
+    "logLevel": "info",
+    "pathRewrite": {"^/uservice/gateway/appcatalog/api": "/api"},
+    "bypass": injectCreds
+  },
+  "/all": {
+    "secure": false,
+    "bypass": redirect
+  },
 }
 
 module.exports = PROXY_CONFIG;
