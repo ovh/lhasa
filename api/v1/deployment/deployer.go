5dea183a8a4cc3a98439caa2f94418ef3e5c7441
Fabien Meurillon <fabien.meurillon@corp.ovh.com>
2018-07-17 15:33:00 +0200

    Deploy release idempotently

--- api/v1/deployment/deployer.go
+++ api/v1/deployment/deployer.go
@@ -7,39 +7,66 @@ import (
 	"github.com/juju/errors"
 	"github.com/ovh/lhasa/api/db"
 	"github.com/ovh/lhasa/api/v1"
+	"github.com/sirupsen/logrus"
 )
 
 // Deployer deploys an application version to the given environment and removes old deployments
-type Deployer func(application v1.Release, environment v1.Environment, deployment *v1.Deployment) error
+type Deployer func(application v1.Release, environment v1.Environment, deployment *v1.Deployment, log *logrus.Entry) (*v1.Deployment, bool, error)
 
 // ApplicationDeployer deploys an application version to the given environment and removes old deployments
 func ApplicationDeployer(tm db.TransactionManager, depFactory RepositoryFactory) Deployer {
-	return func(app v1.Release, env v1.Environment, dep *v1.Deployment) error {
-		return tm.Transaction(func(db *gorm.DB) error {
+	return func(app v1.Release, env v1.Environment, dep *v1.Deployment, log *logrus.Entry) (*v1.Deployment, bool, error) {
+		c := false
+		created := &c
+		var d **v1.Deployment
+		err := tm.Transaction(func(db *gorm.DB) error {
 			depRepo := depFactory(db)
+			j, err := getProperties(dep, app.Domain, app.Name, app.Version, env.Slug)
+			dep.Properties.UnmarshalJSON(j)
+			dep.EnvironmentID = env.ID
+			dep.ApplicationID = app.ID
+
+			// Looking for a previous deployment on the same release / environment
+			prevs, err := depRepo.FindActivesByRelease(app.Domain, app.Name, app.Version, map[string]interface{}{"environment_id": env.ID})
+			if err != nil {
+				log.
+					WithField("domain", app.Domain).
+					WithField("name", app.Name).
+					WithField("version", app.Version).
+					WithField("env", env.Slug).
+					WithError(err).
+					Warnf("was not able to find previous active deployment for this release")
+			}
+			if len(prevs) > 0 {
+				prev := prevs[0]
+				prev.Properties.UnmarshalJSON(j)
+				d = &prev
+				return depRepo.Save(prev)
+			}
 
 			if err := depRepo.UndeployByApplicationEnv(app.Domain, app.Name, env.ID); err != nil {
 				return err
 			}
-			props := map[string]interface{}{}
-			if len(dep.Properties.RawMessage) > 0 {
-				if err := json.Unmarshal(dep.Properties.RawMessage, &props); err != nil {
-					return errors.BadRequestf("properties field should be a valid json object: %s", err.Error())
-				}
-			}
-			props["_app_domain"] = app.Domain
-			props["_app_name"] = app.Name
-			props["_app_version"] = app.Version
-			props["_env_slug"] = env.Slug
-			j, err := json.Marshal(props)
-			if err != nil {
-				return err
-			}
 
-			dep.Properties.UnmarshalJSON(j)
-			dep.EnvironmentID = env.ID
-			dep.ApplicationID = app.ID
+			c := true
+			created = &c
+			d = &dep
 			return depRepo.Save(dep)
 		})
+		return *d, *created, err
 	}
+}
+
+func getProperties(dep *v1.Deployment, domain, name, version, envSlug string) ([]byte, error) {
+	props := map[string]interface{}{}
+	if len(dep.Properties.RawMessage) > 0 {
+		if err := json.Unmarshal(dep.Properties.RawMessage, &props); err != nil {
+			return nil, errors.BadRequestf("properties field should be a valid json object: %s", err.Error())
+		}
+	}
+	props["_app_domain"] = domain
+	props["_app_name"] = name
+	props["_app_version"] = version
+	props["_env_slug"] = envSlug
+	return json.Marshal(props)
 }
